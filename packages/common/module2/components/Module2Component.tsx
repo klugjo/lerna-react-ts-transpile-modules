3f2890238d32c7ddc414e543645fad8e4f58b6a2
Jonathan Klughertz <klughertz.j@gmail.com>
2020-07-03 18:18:16 +0200

    test css module

--- packages/common/module2/components/Module2Component.tsx
+++ packages/common/module2/components/Module2Component.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
+import styles from './test.module.css';
+
 const Module2Component: React.FC = () => {
-  return <p>HEADER</p>;
+  return <p className={styles.test}>HEADER</p>;
 };
 
 export default Module2Component;
